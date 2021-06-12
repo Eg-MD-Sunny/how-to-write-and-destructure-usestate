3c191cfc24fe8419dfddfdb4113544d588cce2be
Naim Zaman <egnaimur@gmail.com>
2021-06-12 20:59:21 +0600

    How To Write & Destructure useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+		<header className="App-header">
+			{/*=====>>Component Call [Start]*/}
+				<MovieCounter></MovieCounter>
+			{/*=====>>Component Call [End]*/}
+
+			
+		</header>
+	</div>
   );
 }
+//=====>>Create Component [Start]
+		function MovieCounter(){
+			// const count = 5;
 
+		//---Example-01
+		const result = useState(0);
+		console.log(result);
+
+		//---Example-02
+		const result_01 = ['abc','def'];
+		console.log(result_01[0], result_01[1]);
+
+		//---Example-03
+		const result_02 = ['Good', 'Bad'];
+		const First = result_02[0];
+		const Second = result_02[1];
+		console.log(First, Second);
+
+		//---Example-04
+		const [first, second] = ['Not', 'Good']
+		console.log(first, second);
+
+		//---Example-05
+		const [one, two] = useState(0)
+		console.log(one, two);
+
+		//---Example-06
+		const [count, setCount] = useState(0)
+		console.log(count, setCount); //======>> useState Declear 
+//======>>onClick Function [Start]
+		const handleClicked = () => {
+			console.log('Someone Clicked Me');
+			const newCount = count + 1;
+			setCount(newCount);
+		}
+//======>>onClick Function [End]
+		return(
+			<div>
+				<button onClick={handleClicked}>Add Movie</button>
+				<h3>Number of Movies : {count}</h3>
+				<MovieDisplay movies={count}></MovieDisplay>
+				<MovieDisplay movies={count + 10}></MovieDisplay>
+				<MovieDisplay movies={count + 5}></MovieDisplay>
+				<MovieDisplay movies={count}></MovieDisplay>
+			</div>
+		)
+	}
+//=====>>Create Component [End]
+
+//=====>>Create Component [Start]
+function MovieDisplay(props){
+	return <h4>Movies I have acted : {props.movies} </h4>
+}
+//=====>>Create Component [End]
 export default App;
